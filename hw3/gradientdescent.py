e644dd0208843436f6081267e39576327571c395
Griffin Solimni <solimini.1@osu.edu>
2017-03-21 22:11:46 -0400

    modules

--- hw3/gradientdescent.py
+++ hw3/gradientdescent.py
@@ -14,71 +14,70 @@ def loss(X, Y, w, N):
 def gradient(X, Y, w, eta, N):
     return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
 
-def train(T, eta):
+def train(gradient, T, eta):
     w = np.zeros((785,1))
     for t in range(0, T):
         w = gradient(training_X, training_Y, w, eta, training_N)
 
     return w
 
-def train_err(w):
+def train_err(loss, w):
     return loss(training_X, training_Y, w, training_N)
 
-def val_err(w):
+def val_err(loss, w):
     return loss(val_X, val_Y, w, val_N)
 
-def test_err(w):
+def test_err(loss, w):
     return loss(test_X, test_Y, w, test_N)
 
-T = 1000
-eta_list = [3.5e-7, 3e-7, 1e-7, 7e-7, 1e-8, 5e-8, 1e-9, 5e-9, 1e-10, 5e-10]
-best_eta = 0 
-lowest_err = float("inf")
+def run_experiment(loss, gradient):
+    T = 1000
+    eta_list = [3.5e-7, 3e-7, 1e-7, 7e-7, 1e-8, 5e-8, 1e-9, 5e-9, 1e-10, 5e-10]
+    best_eta = 0 
+    lowest_err = float("inf")
 
-for eta in eta_list:
-    w = train(T, eta)
-    
-    tr_err = train_err(w)
-    v_err = val_err(w)
+    for eta in eta_list:
+        w = train(gradient, T, eta)
+        
+        tr_err = train_err(loss, w)
+        v_err = val_err(loss, w)
 
-    print "eta: " + str(eta)
-    print "training error: " + str(tr_err)
-    print "validation error: " + str(v_err)
-    print
-    
-    if v_err < lowest_err:
-        best_eta = eta
-        lowest_err = v_err
+        print "eta: " + str(eta)
+        print "training error: " + str(tr_err)
+        print "validation error: " + str(v_err)
+        print
+        
+        if v_err < lowest_err:
+            best_eta = eta
+            lowest_err = v_err
 
-best_T = 1
-lowest_err = float("inf")
+    best_T = 1
+    lowest_err = float("inf")
 
-max_T = 100
+    max_T = 100
 
-T_values = range(1, max_T + 1)
-err_values = [] 
-for T in T_values:
-    w = train(T, best_eta)
-    err = val_err(w)
-    
-    err_values.append(err)
+    T_values = range(1, max_T + 1)
+    err_values = [] 
+    for T in T_values:
+        w = train(gradient, T, best_eta)
+        err = val_err(loss, w)
+        
+        err_values.append(err)
 
-    if err < lowest_err:
-        best_T = T
-        lowest_err = err
+        if err < lowest_err:
+            best_T = T
+            lowest_err = err
 
-    print str(100.0 * T / max_T) + '%'
+        print str(100.0 * T / max_T) + '%'
 
-plt.plot(T_values, err_values, '-')
+    plt.plot(T_values, err_values, '-')
 
-plt.title('Validation Error vs. T Values')
-plt.xlabel('T Value')
-plt.ylabel('Validation Error')
+    plt.title('Validation Error vs. T Values')
+    plt.xlabel('T Value')
+    plt.ylabel('Validation Error')
 
-plt.show()
+    plt.show()
 
-w = train(best_T, best_eta)
-
-w = train(1000, best_eta)
-print test_err(w)
+    w = train(gradient, best_T, best_eta)
+    print test_err(loss, w)
 
--- hw3/logreg.py
+++ hw3/logreg.py
@@ -1,73 +1,11 @@
+import gradientdescent as gd
 import numpy as np
-import os
-import os.path
-import pickle
 
-import setup as s 
+def loss(X, Y, w, N):
+    return np.sum(np.square(X * w - Y)) / N
 
-(training_X, training_Y, training_N, val_X, val_Y, val_N, test_X, test_Y, test_N) = s.run()
+def gradient(X, Y, w, eta, N):
+    return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
 
-def train(T, gamma):
-    w = np.zeros((785,1))
-    for t in range(0, T):
-        h = (training_X * w - training_Y).T
-        w = w - gamma * (2.0/training_N) * (h * training_X).T
-
-    return w
-
-def train_err(w):
-    err = np.square(training_X * w - training_Y)
-    loss = np.sum(err) / training_N
-    return loss
-
-def val_err(w):
-    err = np.square(val_X * w - val_Y)
-    loss = np.sum(err) / val_N
-    return loss
-
-def test_err(w):
-    err = np.square(test_X * w - test_Y)
-    loss = np.sum(err) / test_N
-    return loss
-
-T = 1000
-eta_list = [3.5e-7, 3e-7, 1e-7, 7e-7, 1e-8, 5e-8, 1e-9, 5e-9, 1e-10, 5e-10]
-best_eta = 0 
-lowest_err = float("inf")
-
-for eta in eta_list:
-    w = train(T, eta)
-    
-    tr_err = train_err(w)
-    v_err = val_err(w)
-
-    print "eta: " + str(eta)
-    print "training error: " + str(tr_err)
-    print "validation error: " + str(v_err)
-    print
-    
-    if v_err < lowest_err:
-        best_eta = eta
-        lowest_err = v_err
-
-#  best_T = 1
-#  lowest_err = float("inf")
-#  
-#  T_values = range(1, 1001)
-#  err_list = []
-#  for T in T_values:
-    #  print T
-#  
-    #  w = train(T, best_eta)
-    #  err = val_err(w)
-#      
-    #  err_list.append(err)
-#  
-    #  if err < lowest_err:
-        #  best_T = T
-        #  lowest_err = err
-#  
-#  w = train(best_T, best_eta)
-w = train(1000, best_eta)
-print test_err(w)
+gd.run_experiment(loss, gradient)
 
