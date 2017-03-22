7b655d97c0921dc8f77c43c75e4537e6df67365d
Griffin Solimni <solimini.1@osu.edu>
2017-03-22 00:05:33 -0400

    finals

--- hw3/linreg.py
+++ hw3/linreg.py
@@ -7,5 +7,7 @@ def loss(X, Y, w, N):
 def gradient(X, Y, w, eta, N):
     return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
 
-gd.run_experiment(loss, gradient)
+eta_values = [1e-10, 5e-10, 1e-9, 5e-9, 1e-8, 3.5e-8, 5e-8, 1e-7, 3.5e-7, 5e-7]
+
+gd.run_experiment(loss, gradient, eta_values)
 
--- hw3/logreg.py
+++ hw3/logreg.py
@@ -2,10 +2,14 @@ import gradientdescent as gd
 import numpy as np
 
 def loss(X, Y, w, N):
-    return numpy.log(1 + exp(-w.T * X.T * Y)) 
+    print -w.T * X.T * Y 
+    return np.log(1 + np.exp(-w.T * X.T * Y)) 
 
 def gradient(X, Y, w, eta, N):
-    return w - eta * (1.0 / N) * np.exp(-w.T * X.T * Y) * (-X.T * Y) / (1 + np.exp(-w.T * X.T * Y))
+    h = float(np.exp(-w.T * X.T * Y))
+    return w - eta * (1.0 / N) * h / (1 + h) * (-X.T * Y) 
 
-gd.run_experiment(loss, gradient)
+eta_values = [1e-10, 5e-10, 1e-9, 5e-9, 1e-8, 3.5e-8, 5e-8, 1e-7, 3.5e-7, 5e-7]
+
+gd.run_experiment(loss, gradient, eta_values)
 
