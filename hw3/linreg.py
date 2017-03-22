fd02043d7ce7539e6cd9ab7264dddf6e3ae01d0e
Griffin Solimni <solimini.1@osu.edu>
2017-03-21 22:42:39 -0400

    log stuff

--- hw3/linreg.py
+++ hw3/linreg.py
@@ -2,7 +2,7 @@ import gradientdescent as gd
 import numpy as np
 
 def loss(X, Y, w, N):
-    return np.sum(np.square(X * w - Y)) / N
+    return (X * w - Y).T * (X * w - Y) / N
 
 def gradient(X, Y, w, eta, N):
     return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
--- hw3/logreg.py
+++ hw3/logreg.py
@@ -2,10 +2,10 @@ import gradientdescent as gd
 import numpy as np
 
 def loss(X, Y, w, N):
-    return np.sum(np.square(X * w - Y)) / N
+    return numpy.log(1 + exp(-w.T * X.T * Y)) 
 
 def gradient(X, Y, w, eta, N):
-    return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
+    return w - eta * (1.0 / N) * np.exp(-w.T * X.T * Y) * (-X.T * Y) / (1 + np.exp(-w.T * X.T * Y))
 
 gd.run_experiment(loss, gradient)
 
