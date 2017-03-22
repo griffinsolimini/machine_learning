0a77c4a0ae7cf7d7cf081a07b771723aa64db9e1
Griffin Solimni <solimini.1@osu.edu>
2017-03-22 00:39:33 -0400

    final submission

--- hw3/gradientdescent.py
+++ hw3/gradientdescent.py
@@ -8,35 +8,31 @@ import setup as s
 
 (training_X, training_Y, training_N, val_X, val_Y, val_N, test_X, test_Y, test_N) = s.run()
 
-def loss(X, Y, w, N):
-    return np.sum(np.square(X * w - Y)) / N
-
-def gradient(X, Y, w, eta, N):
-    return w - eta * (2.0 / N) * ((X * w - Y).T * X).T
+def train_increment(gradient, w, eta):
+    return gradient(training_X, training_Y, w, eta, training_N)
 
 def train(gradient, T, eta):
     w = np.zeros((785,1))
     for t in range(0, T):
-        w = gradient(training_X, training_Y, w, eta, training_N)
+        w = train_increment(gradient, w, eta) 
 
     return w
 
 def train_err(loss, w):
-    return loss(training_X, training_Y, w, training_N)
+    return float(loss(training_X, training_Y, w, training_N))
 
 def val_err(loss, w):
-    return loss(val_X, val_Y, w, val_N)
+    return float(loss(val_X, val_Y, w, val_N))
 
 def test_err(loss, w):
-    return loss(test_X, test_Y, w, test_N)
+    return float(loss(test_X, test_Y, w, test_N))
 
-def run_experiment(loss, gradient):
+def run_experiment(loss, gradient, eta_values):
     T = 1000
-    eta_list = [3.5e-7, 3e-7, 1e-7, 7e-7, 1e-8, 5e-8, 1e-9, 5e-9, 1e-10, 5e-10]
     best_eta = 0 
     lowest_err = float("inf")
 
-    for eta in eta_list:
+    for eta in eta_values:
         w = train(gradient, T, eta)
         
         tr_err = train_err(loss, w)
@@ -51,15 +47,19 @@ def run_experiment(loss, gradient):
             best_eta = eta
             lowest_err = v_err
 
-    best_T = 1
+    best_T = 0 
     lowest_err = float("inf")
 
-    max_T = 100
+    max_T = 1000
 
     T_values = range(1, max_T + 1)
     err_values = [] 
+
+    w = np.zeros((785,1))
+
     for T in T_values:
-        w = train(gradient, T, best_eta)
+        w = train_increment(gradient, w, best_eta)
+        
         err = val_err(loss, w)
         
         err_values.append(err)
@@ -68,7 +68,12 @@ def run_experiment(loss, gradient):
             best_T = T
             lowest_err = err
 
-        print str(100.0 * T / max_T) + '%'
+
+    w = train(gradient, best_T, best_eta)
+    
+    print "best T: " + str(best_T)
+    print "best eta: " + str(best_eta)
+    print "test error: " + str(test_err(loss, w))
 
     plt.plot(T_values, err_values, '-')
 
@@ -77,7 +82,3 @@ def run_experiment(loss, gradient):
     plt.ylabel('Validation Error')
 
     plt.show()
-
-    w = train(gradient, best_T, best_eta)
-    print test_err(loss, w)
-
--- hw3/logreg.py
+++ hw3/logreg.py
@@ -2,14 +2,13 @@ import gradientdescent as gd
 import numpy as np
 
 def loss(X, Y, w, N):
-    print -w.T * X.T * Y 
     return np.log(1 + np.exp(-w.T * X.T * Y)) 
 
 def gradient(X, Y, w, eta, N):
     h = float(np.exp(-w.T * X.T * Y))
     return w - eta * (1.0 / N) * h / (1 + h) * (-X.T * Y) 
 
-eta_values = [1e-10, 5e-10, 1e-9, 5e-9, 1e-8, 3.5e-8, 5e-8, 1e-7, 3.5e-7, 5e-7]
+eta_values = [1e-17, 1e-16, 1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8]
 
 gd.run_experiment(loss, gradient, eta_values)
 
